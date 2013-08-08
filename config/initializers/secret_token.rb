8582b20e0bd52b7c33e095e52a1b49d99d2d9422
jason thein <emailmejason@yahoo.com>
2013-08-08 09:40:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caltrain::Application.config.secret_token = 'a466ce6342ababaa91c4819a29d6c2f5428f2b3909d316e37d961faab0248d3aa254d31110d3513efeba723bd2a74c9bd5608bbbb035cb20ad9c3487aa30d908'
