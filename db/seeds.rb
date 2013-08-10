a8792b363c6e164717a10a880e700bc36f359faf
jason thein <emailmejason@yahoo.com>
2013-08-10 11:21:00 -0700

    added directions n and s

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 
 file_path = Rails.root.join("public", "stop_times.csv").to_s
 trains = Roo::Spreadsheet.open(file_path)
@@ -18,15 +9,10 @@ trains_hash = {}
 #     "11:21:00" => "San Jose Caltrain",
 #     "11:22:00" => "Santa Clara Caltrain",
 #     "11:23:00" => "Bayshore Caltrain"
-#   },
-#   "102" => {
-#     "11:21:00" => "San Jose Caltrain",
-#     "11:22:00" => "Santa Clara Caltrain",
-#     "11:23:00" => "Bayshore Caltrain"
 #   }
 # }
 
-def convert_train_string_into_train_number(train_string)
+def shorten_the_train_number(train_string)
 
   train_string[0...3]
 end
@@ -36,7 +22,7 @@ end
 trains.each_with_index({:train => "trip_id", :time => "arrival_time",  :station => "stop_id"}) do |row_hash, i|
   if i > 0
     long_train_number = row_hash[:train]
-    short_train_number = convert_train_string_into_train_number(long_train_number)
+    short_train_number = shorten_the_train_number(long_train_number)
 
     # Set a KEY in the trains_hash for this train - unless that train is already in the trains_hash.
     unless trains_hash.has_key?(short_train_number)
@@ -51,3 +37,22 @@ trains.each_with_index({:train => "trip_id", :time => "arrival_time",  :station 
 end
 
 puts trains_hash
+
+trains_hash.each do |x,y|
+  train=Train.create!(:number=>x,:schedule=>y)
+end
+
+trains=Train.all
+
+trains.each do |x|
+
+  if x.number.to_i % 2 == 0
+    x.update_attribute(:direction,"s")
+  else
+    x.update_attribute(:direction,"n")
+  end
+end
+
+
+
+
